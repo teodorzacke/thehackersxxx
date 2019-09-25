4a6abea8eff42a1c7b4791c6634738512fa5c488
caisakj <55660837+caisakj@users.noreply.github.com>
2019-09-25 16:24:12 +0200

    Add files via upload

--- liinalg task 1.py
+++ liinalg task 1.py
@@ -9,21 +9,44 @@ from scipy import *
 from matplotlib.pyplot import *
 from numpy import *
 
-A = [(1,1,2),
-     (1,2,1),
-     (2,1,1),
-     (2,2,1)]
+A = array([[1,1],
+           [4,-1],
+           [3,2]])
 
-y = [(1),
-     (-1),
-     (1),
-     (-1)]
+y = array([6,8,5])
+
+z = array([1,2])
 
 #method of least squares: At*A=Aty
+#point 1
+def leastsq(A, y):
+    """
+    Setup of functions equivalent to what was to bedone manually
+    when computing the method of least squares.
+    """
+    lhs = np.matmul(np.transpose(A), A)
+    rhs = np.matmul(np.transpose(A), y)
+    sol = np.linalg.solve(lhs, rhs)
+    return sol
 
-lhs = np.matmul(np.transpose(A), A)
-rhs = np.matmul(np.transpose(A), y)
+#point 2
+def leastsq2(x):
+    """
+    directly interpreting (Ax-y)**2 ==> (Ax-y)*(Ax-y)
+    as a matrix multiplication (works exactly like the nympy dot product here)
+    """
+    return np.dot(np.dot(A,x)-y,np.dot(A,x)-y)
+        
+x0 = array([1, 2])   
 
-sol = np.linalg.solve(lhs, rhs)
+print(opt.fmin(leastsq2,x0))
+print(leastsq(A,y))
 
-print(sol)
+#point 3
+
+
+
+
+
+
+
