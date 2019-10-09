dfff6f7f6dcabd1f5306df4258844430bcd61da2
teodor <teodorzacke@gmail.com>
2019-10-09 19:36:13 +0200

    lade till bahhhhh

--- linalgtask2.py
+++ linalgtask2.py
@@ -21,23 +21,14 @@ A = np.array([[1,3,2],
 
 z0 = np.array([8,3,12])
 z01 = z0
-listt1 = []
+listt1 = [z0]
 for i in range(iterates):
     newterm = np.dot(A, z01)
     listt1.append(newterm)
     z01 = newterm
-    
 
-X = []
-Y = []
-Z = []
-for i in listt1:
-    X.append(i[0])
-    Y.append(i[1])
-    Z.append(i[2])
-fig = plt.figure()
-ax = p3.Axes3D(fig)
-ax.plot_trisurf(X,Y,Z)
+
+
 
 ### 2.2
 z02 = z0
@@ -45,10 +36,23 @@ listt2 = []
 for i in listt1:
     listt2.append(i / np.linalg.norm(i))
 cz = listt2[-1]
+
+diffl = []
+for i in range(len(listt2)-1):
+    diffl.append(np.linalg.norm(listt2[i] - listt2[i-1]))
+    
+#X, Y, Z = list(zip(*listt2))
+#fig = plt.figure()
+#ax = p3.Axes3D(fig)
+#ax.plot(X,Y,Z)
+#plt.plot(range(iterates+1), X)
+#plt.plot(range(iterates+1), Y)
+#plt.plot(range(iterates+1), Z)
+
 ### 2.3
-listt3 = []
-for i in listt2:
-    listt3.append(np.dot(np.dot(np.transpose(i), A), i))
+#listt3 = []
+#for i in listt2:
+#    listt3.append(np.dot(np.dot(np.transpose(i), A), i))
 
 ### 2.4
 #z04 = z0
